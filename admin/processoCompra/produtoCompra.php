f78bbaaba300bc04532cef1ad8eed02c5ac520dd
Rafael <raphaelldff@gmail.com>
2021-04-29 18:36:52 -0300

    controle de estoque

--- admin/processoCompra/produtoCompra.php
+++ admin/processoCompra/produtoCompra.php
@@ -1,1 +1,10 @@
-<h1>teste</h1>
+<h1>----------Cadastro - FORM -------------</h1>
+<h1>---------------------------------------</h1>
+<h1>ID nome do Fornecedor - SELECT no banco</h1>
+<h1>Nome do Produto - CADASTRO</h1>
+<h1>ID da marca - SELECT no banco</h1>
+<h1>data do cadastro - CADASTRO</h1>
+<h1>quantidade em estoque - CADASTRO</h1>
+<h1>valor bruto - CADASTRO</h1>
+<h1>codigo do produto - CADASTRO</h1>
+
