5d93d89e3c1bd989850484e580b5907ac3746eaf
Rafael <raphaelldff@gmail.com>
2021-05-25 17:53:59 -0300

    produto

--- loja/pages/produto.php
+++ loja/pages/produto.php
@@ -1,6 +1,5 @@
 <?php
     if ( !empty ( $id ) ) {
-
     	$sql = "SELECT * 
     	FROM produto
     	WHERE id = :id LIMIT 1";
@@ -15,7 +14,6 @@
 		$venda_unitaria 	  = $dados->venda_unitaria;
 		$id = "";
 	}	
-
     if ( empty ( $promocao ) ) {
 		//1499.99 -> 1.499,99
 		$venda_unitaria = "R$ " . number_format($venda_unitaria, 2, ",", ".");
@@ -28,28 +26,30 @@
 	}
 ?>
 <div class="card">
-  <div class="row no-gutters mt-3">
+  <div class=" mt-3">
     <div class="col-md-5" >
 		<a href="<?=$fotog?>" data-lightbox="foto" title="<?=$dados->nome_produto?>">
         	<img src="<?=$foto?>" alt="<?=$dados->nome_produto?>" width="100%" height="100%">
       	</a>
     </div>
-    <div class="col-md-7">
-      <div class="card-body">
-		<h5 class="card-title venda_unitaria"><strong><?=$dados->nome_produto?></strong></h5><br>
-		<p>Código do Produto: <?=$dados->codigo?></p>
-		<br>
-		<p class="venda_unitaria"><strong>Valor: R$ <?=$dados->venda_unitaria;?></strong></p>
-		<a href="pages/carrinho/<?=$dados->id;?>" class="btn btn-success">Adicionar ao Carrinho</a><br>
-		
-		<br>  
-	</div>
+    <div class="col-md-7 mb-5">
+      	<div class="card-body">
+			<h4 class="card-title venda_unitaria"><strong><?=$dados->nome_produto?></strong></h4>
+			<p>Código do Produto: <?=$dados->codigo?></p>
+			<h5 class="venda_unitaria mt-5"><strong>Valor: R$ <?=$dados->venda_unitaria;?></strong></h5>
+			<a href="pages/carrinho/<?=$dados->id;?>" class="btn btn-success float-end mt-5">Adicionar ao Carrinho</a><br>  
+		</div>
     </div>
-	<div class="container-fluid mt-5">
-	<div class="row col-12">
-	<p class="card-body"><strong>Descrição:</strong> <?=$dados->descricao;?></p>
-	<p class="card-body"><?=$dados->espec_tecnica;?></p>
-	</div>
-	</div>
   </div>
+</div>
+<div class="container mt-5">
+	<div class="row">
+		<h3 class="text-center"><strong>Descrição do Produto:</strong></h3>
+		<p><?=$dados->descricao;?></p>	
+	</div>
+</div>
+<div class="container-fluid mt-4"> 
+	<div class="row">
+		<p><?=$dados->espec_tecnica;?></p>
+	</div>
 </div>
